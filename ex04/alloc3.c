3a5e17f96990b315d40d6d35ae5e969a15efdc00
Crow314 <master@crow31415.net>
2023-11-07 11:26:56 +0900

    Copy to alloc3 from alloc2

--- ex04/alloc3.c
+++ ex04/alloc3.c
@@ -22,12 +22,52 @@ static HEADER *allocp = &allocbuf;	/* Last block allocated */
 
 void *alloc3(int nbytes)		/* Return pointer to nbytes block */
 {
-  /* FILL HERE */
+  HEADER *p, *q;
+  int nunits = 1 + (nbytes + sizeof(HEADER) - 1) / sizeof(HEADER);
+
+  for (q = allocp, p = q->s.ptr; ; q = p, p = p->s.ptr) {
+    if (p->s.size >= nunits) {
+      if (p->s.size == nunits) { /* Just */
+        if (p == q) { /* We have only one element */
+          return 0;
+        }
+        q->s.ptr = p->s.ptr;
+      } else { /* Allocate tail */
+        p->s.size -= nunits;
+        p += p->s.size;
+        p->s.size = nunits;
+      }
+      allocp = q;
+      return (void *)(p + 1);
+    }
+    if (p == allocp) {
+      return 0;
+    }
+  }
 }
 
 /* afree3() is Exactly same as afree2() */
 
 void afree3(void *ap)		/* Free space pointed to by p */
 {
-  /* omitted */
+  HEADER *p, *q;
+  p = (HEADER *)ap - 1;
+  for (q = allocp; !(p > q && p < q->s.ptr); q = q->s.ptr) {
+    if (q >= q->s.ptr && (p > q || p < q->s.ptr)) {
+      break;
+    }
+  }
+  if (p + p->s.size == q->s.ptr) { /* Merge to upward */
+    p->s.size += q->s.ptr->s.size;
+    p->s.ptr = q->s.ptr->s.ptr;
+  } else {
+    p->s.ptr = q->s.ptr;
+  }
+  if (q + q->s.size == p) { /* Merge to downward */
+    q->s.size += p->s.size;
+    q->s.ptr = p->s.ptr;
+  } else {
+    q->s.ptr = p;
+  }
+  allocp = q;
 }
